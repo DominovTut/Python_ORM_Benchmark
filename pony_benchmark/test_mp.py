2f797529dbed05d921141e762187fbb6c6e8f203
dominov <dominovtut@gmail.com>
2020-03-25 14:14:43 +0300

    Added 'test_controler' process in 'test_mp.py'

--- pony_benchmark/test_mp.py
+++ pony_benchmark/test_mp.py
@@ -6,9 +6,23 @@ from random import randint
 from multiprocessing import Process, Value
 
 
-def test(cnt, start, now):
+def test_controler(cnt, run, start, gl_start):
+	while run.value:
+		now = time.time()
+		if now - gl_start >= 61:
+			run.value = False
+		if now - start.value >= 10:
+			print(cnt.value)
+			with cnt.get_lock():
+				cnt.value = 0
+			with start.get_lock():
+				start.value = now
+
+		
+
+def test(cnt, run):
 	now = time.time()
-	while now - gl_start < 601:
+	while run.value:
 		choice = randint(1, 100)
 		if choice <= 45:
 			tran = [new_order_tran, {'w_id' : randint(1, 5), 'c_id' : randint(1, 10)}]
@@ -23,15 +37,11 @@ def test(cnt, start, now):
 		for i in range(10):
 			try:
 				tran[0](**tran[1])
-				cnt.value += 1
+				with cnt.get_lock():
+					cnt.value += 1
 				break
 			except:
 				continue
-		now = time.time()
-		if now - start.value >= 60:
-			print(cnt.value)
-			cnt.value = 0
-			start.value = now
 
 
 
@@ -41,11 +51,15 @@ if __name__ == '__main__':
 	start = Value('d', 0.0)
 	processes = []
 	start.value = gl_start = time.time()
+	run = Value('b', True)
 
 	for i in range(2):
-		process = Process(target=test, args=(cnt, start, gl_start))
+		process = Process(target=test, args=(cnt, run))
 		process.start()
 		processes.append(process)
+	process = Process(target=test_controler, args=(cnt, run, start, gl_start))
+	process.start()
+	processes.append(process)
 
 
 	for process in processes:
