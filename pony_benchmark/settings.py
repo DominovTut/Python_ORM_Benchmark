da3593cf54c9adbabee572a8ec5f313fb1e4d7f1
dominov <dominovtut@gmail.com>
2020-03-28 21:23:43 +0300

    Added settings.py for pony and alchemy

---
+++ pony_benchmark/settings.py
@@ -0,0 +1,13 @@
+PROVIDER = {
+    'postgres':{
+        'provider':'postgres', 
+        'user':'benchmark', 
+        'password':'bench', 
+        'host':'localhost', 
+        'database':'benchmark'},
+}
+
+AMOUNT_OF_WAREHOUSES = 10
+AMOUNT_OF_PROCESSES = 2
+TEST_DURATION = 61
+PRINT_INTERVAL = 10
---
+++ sqlalchemy_benchmark/settings.py
@@ -0,0 +1,8 @@
+PROVIDER = {
+    'postgres':"postgresql://bench_sa:alchemy@localhost/bench_sa",
+}
+
+AMOUNT_OF_WAREHOUSES = 10
+AMOUNT_OF_PROCESSES = 2
+TEST_DURATION = 61
+PRINT_INTERVAL = 10
